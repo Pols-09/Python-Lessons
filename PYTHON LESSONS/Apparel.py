ba42191f843b134731aa2deb86f39f3563925289
Pols09 <polsdeku09@gmail.com>
2018-06-22 13:41:13 -0700

    Shop sample

---
+++ PYTHON LESSONS/Apparel.py
@@ -0,0 +1,20 @@
+print ("I need to stock new stuff for the shop:")
+
+print ("Tees"), 35 * 20 / 7
+print ("Trousers"), 40 * 10 / 7
+
+print ("Now i calculate for next week:")
+
+print (35 + 40 * 30 / 7)
+
+Tees = 20
+cost_of_tees = 30
+delivery = 5
+trousers = 10
+cost_of_trousers = 20
+caps = 5
+cost_of_caps = 15
+print ("There are", Tees, "tees available atm.")
+print ("Tees go for", cost_of_tees + delivery, "at most") 
+print ("Trousers go for", cost_of_trousers + delivery, "at most")
+print ("Caps go for", cost_of_caps, "always")
