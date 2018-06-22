b2a61a78e0243454f51072cadea9bc4c39d80935
Pols09 <polsdeku09@gmail.com>
2018-06-22 13:45:28 -0700

    Shop briefing.

---
+++ PYTHON LESSONS/ApparelDes.py
@@ -0,0 +1,9 @@
+shop_name = 'The ApparelShop'
+goods = 'unisex clothing at affordabl prices'
+location = 'currently available only online'
+delivery = 'delivery is optional but  comes at a charge'
+
+print ("Let's talk about %s." %shop_name)
+print ("We provide %s." %goods)
+print ("We are ", location, "via insta & twitter")
+print (" We offer delivery at but %s." %delivery)
